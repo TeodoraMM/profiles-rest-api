78a999ae80655a31e7108f20c03a4ec1d326e297
TeodoraMM <teodora_mezei12@yahoo.com>
2023-08-25 21:01:17 +0300

    Added viewsets to profiles api

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import viewsets
 
 from profiles_api import serializers
 
@@ -44,3 +45,48 @@ class HelloApiView(APIView):
     def delete(self,request, pk=None):
         """Handle deleting an object """
         return Response({'method':'DELETE'})
+
+
+class HelloViewSet(viewsets.ViewSet):
+    """ TEST API ViewSet """
+    serializer_class =serializers.HelloSerializer
+
+    def list(self, request):
+        """ Return a hello message"""
+        a_viewset=[
+            'User actions (list,crate,update,partial_update)',
+            'Automatically maps to URLs using Routers',
+            'Provides more functionality with less code'
+        ]
+
+        return Response({'message':'Hello!','a_viewset':a_viewset})
+
+    def create(self,request):
+        """ Create a new hello message"""
+        serializer=self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message= f'Hello {name}!'
+            return Response({'message':message})
+        else:
+            return Response(
+                serializer.errors,
+                status = status.HTTP_400_BAD_REQUEST
+            )
+
+    def retrieve(self,response,pk=None):
+        """ Handle gettind an obj by its ID"""
+        return Response({'http_method':'GET'})
+
+    def update(seld,request,pk=None):
+        """Handle updating an obj"""
+        return Response({'http_method':'PUT'})
+
+    def partial_update(seld,request,pk=None):
+        """Handle updating a part of an obj"""
+        return Response({'http_method':'PATCH'})
+
+    def destroy(seld,request,pk=None):
+        """Handle removing an obj"""
+        return Response({'http_method':'DELETE'})
