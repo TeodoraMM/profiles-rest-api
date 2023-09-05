57dfe2597cb53112580e7d8ccabfe42e2fa45fca
TeodoraMM <teodora_mezei12@yahoo.com>
2023-09-05 17:18:07 +0300

    aws services

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ybr1wtx+&j%f^@f9(h^*)+ceb_oxbr=x*e9a+wbk6e9$mtme)j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG',1)))
 
 ALLOWED_HOSTS = []
 
@@ -123,3 +123,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'profiles_api.UserProfile'
+
+STATIC_ROOT = 'static/'
